3201fde89cc46c949d9fd66f89b040fd9514794a
UpasanaRani <upasana00078@gmail.com>
2025-12-03 17:55:45 +0530

    Add files via upload

---
+++ syntax.java
@@ -0,0 +1,7 @@
+public class syntax {
+    public static void main(String[] args) {
+        System.out.println("hello,World");
+        // prints hello world as output
+    }
+
+}
---
+++ variables.java
@@ -0,0 +1,12 @@
+// create variables for name,age,and percentage and print them.
+public class variables {
+    public static void main(String[] args) {
+        String name = "Upasana";
+        int age = 20;
+        double percentage = 75.67;
+        System.out.println("Your name is:" + name);
+        System.out.println("Your age is:" + age);
+        System.out.println("Your Percentage:" + percentage);
+    }
+
+}
