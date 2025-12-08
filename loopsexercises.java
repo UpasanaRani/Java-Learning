f46c4015cb4b856dc8d36e048f3d2cbec869a2c4
UpasanaRani <upasana00078@gmail.com>
2025-12-08 19:55:29 +0530

    Add files via upload
    only done one star pattern problem today(not feeling much productive today  ).
    
    will comeback soon!

---
+++ loopsexercises.java
@@ -0,0 +1,15 @@
+public class loopsexercises {
+    public static void main(String[] args) {
+        // int n = 4;// because of 4 lines
+        // for (int i = n; i > 0; i--) {
+        // for (int j = 0; j < i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.print("\n");
+        // }
+
+        // practice problem 2
+
+    }
+
+}
