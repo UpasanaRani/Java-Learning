06d6f21078aebc599624140133862b8e36508a89
UpasanaRani <upasana00078@gmail.com>
2025-12-18 18:28:51 +0530

    Add files via upload
    solved problem 2 and updated.

--- arrayspracticequestions.java
+++ arrayspracticequestions.java
@@ -2,11 +2,24 @@ public class arrayspracticequestions {
     public static void main() {
         // problem 1
         // print all elements of the array
-        float arr[] = { 10.23f, 20.45f, 30.89f, 40.56f, 50.78f };
-        // int n = arr.length;
-        for (float element : arr) {
-            System.out.println(element);
+        // float arr[] = { 10.23f, 20.45f, 30.89f, 40.56f, 50.78f };
+
+        // //this is for each loop which goes through evrery element of an array without
+        // indexes
+        // for (float element : arr) {
+
+        // System.out.println(element);
+        // }
+
+        // problem 2
+        // find sum of elements
+        int arr[] = { 2, 3, 4, 5 };
+        int sum = 0;
+        for (int x : arr) {
+            sum += x;
+
         }
+        System.out.println(sum);
 
     }
 
