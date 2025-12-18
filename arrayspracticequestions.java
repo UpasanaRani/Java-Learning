936319d9ce305181afd52d881b48cc52b38cbaaf
UpasanaRani <upasana00078@gmail.com>
2025-12-18 18:23:46 +0530

    Add files via upload
    did problem 1 today i.e. print all elements of an array.

---
+++ arrayspracticequestions.java
@@ -0,0 +1,13 @@
+public class arrayspracticequestions {
+    public static void main() {
+        // problem 1
+        // print all elements of the array
+        float arr[] = { 10.23f, 20.45f, 30.89f, 40.56f, 50.78f };
+        // int n = arr.length;
+        for (float element : arr) {
+            System.out.println(element);
+        }
+
+    }
+
+}
